e385cfb58510caed9f216b4029d95dab2367f813
pforsber1 <131309258+pforsber1@users.noreply.github.com>
2023-11-26 19:13:58 -0500

    Implement computerChoice function
    The fucntion takes no input and returns a string of either rock, paper,
    or scissors.

--- myScripts.js
+++ myScripts.js
@@ -1,8 +1,22 @@
 function getComputerChoice() {
     //get random number
+    let randNum = Math.random();
+    randNum = (randNum * 10) % 3;
+    randNum = Math.round(randNum);
+    let computerSelection = "";
+
     //select the corresponding string for the number
         //0-rock, 1-paper, 2-scissors
+    if (randNum === 0) {
+        computerSelection = "rock";
+    } else if (randNum === 1) {
+        computerSelection = "paper";
+    } else {
+        computerSelection = "scissors";
+    }
+
     //return selection
+    return computerSelection
 }
 
 function playRound(playerSelection, computerSelection) {
