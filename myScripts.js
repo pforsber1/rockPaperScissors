c9290488b264a3c688c2ee66b143bc6afe8ba95c
pforsber1 <131309258+pforsber1@users.noreply.github.com>
2023-11-26 19:20:00 -0500

    Implement playRound function
    The function takes two parameters: playerSelection, computerSelection.
    The function returns a string stating if the player won or lost and why
    the player lost.
    
    The function uses greater and less than operators to compare the two
    strings. Whichever has a greater length wins that round.

--- myScripts.js
+++ myScripts.js
@@ -21,8 +21,13 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     //compare length of string, whichever is greater wins
-    //select string to return (win or lose)
-    //return string
+    if (playerSelection > computerSelection) {
+        return "You won! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection;
+    } else if (playerSelection < computerSelection) {
+        return "You lost! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection;
+    } else {
+        return "It's a tie!"
+        }
 }
 
 function game() {
