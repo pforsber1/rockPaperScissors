492976d0e5d9b9bf76a54f21bfe3af9cc943ff34
pforsber1 <131309258+pforsber1@users.noreply.github.com>
2023-11-26 19:05:49 -0500

    Create boilerplate and skeleton of myScripts.js
    The boilerplate in index.html links to myScripts.js.
    
    The JavaScript skeleton includes the three functions to be used for
    gameplay: computerSelection, playRound, game. Each function has comments
    to roughly walk through the steps each function has to take.

---
+++ myScripts.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+    //get random number
+    //select the corresponding string for the number
+        //0-rock, 1-paper, 2-scissors
+    //return selection
+}
+
+function playRound(playerSelection, computerSelection) {
+    //compare length of string, whichever is greater wins
+    //select string to return (win or lose)
+    //return string
+}
+
+function game() {
+    //get user input
+    //play five rounds
+        //output message for each round
+    //output overall winner
+}
